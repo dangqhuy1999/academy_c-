94fad60fb74ad510566c5082574777102467215e
huydang <dangqhuy1999@gmail.com>
2025-10-31 08:31:56 +0700

    311025

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,15 +2,16 @@
 #include <vector>
 #include <string>
 
+/*
+  a[n]
+*/
 int main()
 {
     std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
     for (const std::string& word : msg)
     {
         std::cout << word << " ";
     }
     std::cout << std::endl;
-
     return 0;
 }
---
+++ learnStep10_array2dflex.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n,q,k, qi, qj;
+    std::vector<std::vector<int>> a;
+    std::cin>> n>> q;
+    
+    for(int i =0;i<n;i++){
+        std::vector<int> x;
+        std::cin>>k;
+        for (int j =0;j<k;j++) {
+            int value;
+            std::cin>> value;
+            x.push_back(value);
+        }
+        a.push_back(std::move(x));
+    }
+    std::vector<int> showme;
+    for(int i = 0;i<q;i++){
+        std::cin>>qi>>qj;
+        std::cout << a[qi][qj] << '\n';
+    }
+    return 0;
+}
