6628616ee9e54ccc6ecf581df2c6adbffd12bcbe
huydang <dangqhuy1999@gmail.com>
2025-11-03 09:12:38 +0700

    31125_1_SQL

---
+++ sql/oracle_pl/sql_enabled_3_basic.sql
@@ -0,0 +1,23 @@
+SET NULL "NULL";
+SET FEEDBACK OFF;
+SET ECHO OFF;
+SET HEADING OFF;
+SET WRAP OFF;
+SET LINESIZE 10000;
+SET TAB OFF;
+SET PAGES 0;
+SET DEFINE OFF;
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+
+Query  the list of CITY names starting with vowels
+(a,e,i, o or u) from STATION.
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%';
+
+exit;
