e3cb19af9c2d3ba069b719c4b0bef2fb6a9f89a9
huydang <dangqhuy1999@gmail.com>
2025-11-03 22:39:38 +0700

    huydangnight31125

---
+++ sql/oracle_pl/sql_enabled_2.sql
@@ -0,0 +1,26 @@
+
+SET NULL "NULL";
+SET FEEDBACK OFF;
+SET ECHO OFF;
+SET HEADING OFF;
+SET WRAP OFF;
+SET LINESIZE 10000;
+SET TAB OFF;
+SET PAGES 0;
+SET DEFINE OFF;
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+The STATION table is described as follows:
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+For example, if there are three records in the table with CITY values 'New York', 'New York', 'Bengalaru', there are 2 different city names: 'New York' and 'Bengalaru'. The query returns , because .
+
+total number of records - number of unique city names = 3-2 =1 
+*/
+
+select count(CITY) - count(distinct(CITY)) from STATION;
+
+exit;
