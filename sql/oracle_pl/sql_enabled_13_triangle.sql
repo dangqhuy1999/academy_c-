e25cb2a2681f62d461841d30faf2ac5cb0c669aa
huydang <dangqhuy1999@gmail.com>
2025-11-06 16:02:22 +0700

    huydanglunch61125night

---
+++ sql/oracle_pl/sql_enabled_13_triangle.sql
@@ -0,0 +1,37 @@
+SET NULL "NULL";
+SET FEEDBACK OFF;
+SET ECHO OFF;
+SET HEADING OFF;
+SET WRAP OFF;
+SET LINESIZE 10000;
+SET TAB OFF;
+SET PAGES 0;
+SET DEFINE OFF;
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+
+Write a query 
+identifying the type of each record
+in the Triangles table
+using its three side lengths.
+Output one of the following statements
+for each record in the table:
+"Equilateral Triangle" - if all three sides are equal
+"Isosceles Triangle" - if exactly two sides are equal
+"Scalene Triangle" - if no sides are equal
+"Invalid Triangle" - if the given side lengths cannot form a triangle
+
+*/
+
+
+SELECT CASE
+           WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+           WHEN A = B AND B = C THEN 'Equilateral'
+           WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+           ELSE 'Scalene'
+       END AS TriangleType
+FROM Triangles;
+
+exit;
