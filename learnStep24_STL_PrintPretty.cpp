fa3977d7a00e1d54b4bb5b9b3e29b6c069770a2c
huydang <dangqhuy1999@gmail.com>
2025-12-11 13:49:39 +0700

    111225

--- learnStep24_STL_PrintPretty.cpp
+++ learnStep24_STL_PrintPretty.cpp
@@ -38,6 +38,34 @@ int main() {
         of exactly nine decimal places,
         expressed in scientific notation
         using upper case.
+    
+    Cheat Sheet:
+
+    ---------------|--------------------|--------------|--------------
+    Nhóm chức năng |Lệnh(ví dụ)         |Cần include   |Mục đích
+    ---------------|--------------------|--------------|--------------
+    Hệ cơ số       | std::hex,          |              |Chuyển đổi giữa hệ thập lục phân, 
+                   | std::dec, std::oct | <iostream>   |thập phân, bát phân.
+    ---------------|--------------------|--------------|--------------
+    Chỉnh định dạng| std::setw(n)       | <iomanip>    |Đặt độ rộng tối thiểu cho lần in tiếp theo
+                   | std::setfill(c)    |              |Đặt ký tự điền vào để lấp đầy độ rộng
+    ---------------|--------------------|--------------|--------------
+    Căn lề         | std::left          | <iostream>   |Căn lề trái
+                   | std::right         |              |Căn lề phải
+                   | std::internal      |              |Căn giữa dấu hiệu và số
+    ---------------|--------------------|--------------|--------------
+    Biểu diễn số   | std::scientific    | <iomanip>    |Định dạng số theo ký hiệu khoa học(1.23E+04)
+                   | std::fixed         |              |Định dạng số theo dấu thập phân cố định(12345.67)
+                   | std::setprecision(n)|             |Đặt số chữ số thập phân hiển thị
+                   | std::showpos       |              |Hiển thị dấu cộng cho số dương, dấu trừ cho số âm
+                   | std::noshowpos     |              |Không hiển thị dấu 
+    ---------------|--------------------|--------------|--------------
+    Tiền tố số     | std::showbase      | <iostream>   |Hiển thị tiền tố hệ cơ số [0x(hex), 0b(bin), 0(oct)]
+                   | std::noshowbase    |              |Không hiển thị tiền tố hệ cơ số
+    ---------------|--------------------|--------------|--------------
+    Ẩn hiện        | std::uppercase     | <iostream>   |Hiển thị chữ hoa (A-F, E)
+    chữ hoa        | std::nouppercase   |              |Hiển thị chữ thường (a-f, e)
+    ---------------|--------------------|--------------|--------------
     */ 
 
     int T; std::cin >> T;
@@ -45,15 +73,17 @@ int main() {
 	std::cout << std::setw(0xf) << std::internal;
 	while(T--) {
 		double A; std::cin >> A;
-		//double B; std::cin >> B;
+		double B; std::cin >> B;
 		//double C; std::cin >> C;
         long long truncatedA = static_cast<long long>(A);
-        std::cout   << std::dec << std::noshowbase << std::setw(0); 
+        std::cout   << std::dec << std::noshowbase << std::setw(2); 
         std::cout   << std::hex // Set Output to hexadecimal
                     << std::showbase // Show the "0x" prefix 
                     << std::nouppercase // Lowercase letters
                     << truncatedA // The truncated number
                     << std::endl;
+        
+
 	}               
 	return 0;
 }
